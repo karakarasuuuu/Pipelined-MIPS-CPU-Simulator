1dd31841c07ef2846eb957f83b8740a3f392c66d
karasu@MSi <GTR12534@Gmail.com>
2021-01-12 15:50:55 +0800

    completed...?

--- main.py
+++ main.py
@@ -29,15 +29,71 @@ if __name__ == '__main__':
     # process
     while True:
     
-        stall = False
+        # stall = False
 
         # instructions after ID will not be affected
         WB = MEM
         MEM = EX
-        EX = ''
+
+        EX = ID if not stall else ''
+        # registers and memory process if no stall
+        if EX and not stall:
+            # store words from register to memory
+            if EX[0] == 'sw':
+                rs = int(EX[1].strip('$, '))
+                
+                offset = int(EX[2][:EX[2].find('(')]) // 4 # 24($0) -> [0, 2)
+                base = int(EX[2][EX[2].find('$') + 1:EX[2].find(')')]) # 24($0) -> [3, 5)
+
+                mem[base + offset] = reg[rs]
+            # load words from memory to register
+            elif EX[0] == 'lw':
+                rs = int(EX[1].strip('$, '))
+                
+                offset = int(EX[2][:EX[2].find('(')]) // 4 # 24($0) -> [0, 2)
+                base = int(EX[2][EX[2].find('$') + 1:EX[2].find(')')]) # 24($0) -> [3, 5)
+
+                reg[rs] = mem[base + offset]
+            # addition
+            elif EX[0] == 'add':
+                rd = int(EX[1].strip('$, '))
+                rs = int(EX[2].strip('$, '))
+                rt = int(EX[3].strip('$, '))
+
+                reg[rd] = reg[rs] + reg[rt]
+            # subtraction
+            elif EX[0] == 'sub':
+                rd = int(EX[1].strip('$, '))
+                rs = int(EX[2].strip('$, '))
+                rt = int(EX[3].strip('$, '))
+
+                reg[rd] = reg[rs] - reg[rt]
+            # condition
+            elif EX[0] == 'beq':
+                rs = int(EX[1].strip('$, '))
+                rt = int(EX[2].strip('$ ,'))
+                target = int(EX[3].strip('$, '))
+
+                if reg[rs] == reg[rt]:
+                    ID = ''
+                    current = inst.index(EX) + target + 1
+                    IF = inst[current] if len(inst) > current else ''
+                    current += 1
+                    # if taken, we have to restore the values when beq is fetched
+                    for i in range(1, len(reg)): # since $0 is always zero
+                        if i in dreg.keys(): reg[i] = dreg[i]
+                        elif reg[i] != 1: reg[i] = 1
+                    dreg.clear()
+
+                    for i in range(len(mem)):
+                        if i in dmem.keys(): mem[i] = dmem[i]
+                        elif mem[i] != 1: mem[i] = 1
+                    dmem.clear()
 
         # check if stall is needed
+        if not stall: ID = IF
         if ID:
+            stall = False
             if ID[0] == 'add' or ID[0] == 'sub': 
                 rd = ID[1].strip(',')
                 rs = ID[2].strip(',')
@@ -90,9 +146,9 @@ if __name__ == '__main__':
                 #     if rs == WB[1].strip(',') or rt == WB[1].strip(','):
                 #         stall = True
 
-                # if MEM and MEM[0] != 'beq' and MEM[0] != 'sw':
-                #     if rs == MEM[1].strip(',') or rt == MEM[1].strip(','):
-                #         stall = True
+                if MEM and MEM[0] == 'lw':
+                    if rs == MEM[1].strip(',') or rt == MEM[1].strip(','):
+                        stall = True
                 
                 if EX and EX[0] == 'lw':
                     if rs == EX[1].strip(',') or rt == EX[1].strip(','):
@@ -100,64 +156,10 @@ if __name__ == '__main__':
 
         # no stalls then move on
         if not stall:
-            EX = ID
-            ID = IF
+            # EX = ID
+            # ID = IF
             IF = inst[current] if len(inst) > current else ''
             current += 1
-
-        # registers and memory process if no stall
-        if EX:
-            # store words from register to memory
-            if EX[0] == 'sw':
-                rs = int(EX[1].strip('$, '))
-                
-                offset = int(EX[2][:EX[2].find('(')]) // 4 # 24($0) -> [0, 2)
-                base = int(EX[2][EX[2].find('$') + 1:EX[2].find(')')]) # 24($0) -> [3, 5)
-
-                mem[base + offset] = reg[rs]
-            # load words from memory to register
-            elif EX[0] == 'lw':
-                rs = int(EX[1].strip('$, '))
-                
-                offset = int(EX[2][:EX[2].find('(')]) // 4 # 24($0) -> [0, 2)
-                base = int(EX[2][EX[2].find('$') + 1:EX[2].find(')')]) # 24($0) -> [3, 5)
-
-                reg[rs] = mem[base + offset]
-            # addition
-            elif EX[0] == 'add':
-                rd = int(EX[1].strip('$, '))
-                rs = int(EX[2].strip('$, '))
-                rt = int(EX[3].strip('$, '))
-
-                reg[rd] = reg[rs] + reg[rt]
-            # subtraction
-            elif EX[0] == 'sub':
-                rd = int(EX[1].strip('$, '))
-                rs = int(EX[2].strip('$, '))
-                rt = int(EX[3].strip('$, '))
-
-                reg[rd] = reg[rs] - reg[rt]
-            # condition
-            elif EX[0] == 'beq':
-                rs = int(EX[1].strip('$, '))
-                rt = int(EX[2].strip('$ ,'))
-                target = int(EX[3].strip('$, '))
-
-                if reg[rs] == reg[rt]:
-                    ID = ''
-                    current = inst.index(EX) + target + 1
-                    IF = inst[current] if len(inst) > current else ''
-                    current += 1
-                    # if taken, we have to restore the values when beq is fetched
-                    for i in range(1, len(reg)): # since $0 is always zero
-                        if i in dreg.keys(): reg[i] = dreg[i]
-                        elif reg[i] != 1: reg[i] = 1
-                    dreg.clear()
-
-                    for i in range(len(mem)):
-                        if i in dmem.keys(): mem[i] = dmem[i]
-                        elif mem[i] != 1: mem[i] = 1
-                    dmem.clear()
                     
         # control signals
         if EX:
